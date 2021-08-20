59e11ba6bd4b99c9d08460e1061264cad69d9791
Unacon <joaovitor_892@hotmail.com>
2021-08-20 20:44:44 -0300

    Iniciando projeto Locadora Veículo

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LocadoraVeiculos
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocadoraVeiculos.MvcApplication" Language="C#" %>
