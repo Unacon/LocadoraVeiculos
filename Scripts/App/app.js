05a35b9d1a4eefa3d455681c589c9e95c7303e46
Unacon <joaovitor_892@hotmail.com>
2021-08-21 21:32:59 -0300

    Atualização do menu principal, adicionado app.js

---
+++ Scripts/App/app.js
@@ -0,0 +1,13 @@
+﻿/*
+ *  Menu Dropdown
+ */
+$(function () {
+    $('.dropdown').hover(
+        function () {
+            $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn();
+        },
+        function () {
+            $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut();
+        }
+    );
+})
